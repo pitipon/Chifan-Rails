aa7d254dd2941d3f6f93251af6478010d237f043
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-05-12 12:08:39 +0800

    restaurant get all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index ]
+    end
+  end
+
 end
