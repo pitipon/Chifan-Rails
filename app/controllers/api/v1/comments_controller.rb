c06780ec7ba205fa2c071f1642e7849fd8f8375d
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-05-12 16:30:54 +0800

    api for comment include show, edit, update, delete .. that is done

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::CommentsController < Api::V1::BaseController
+    before_action :set_comment, only: [:edit, :update, :show, :destroy]
 
     def index
         @comments = Comment.where(restaurant_id: params[:restaurant_id]).order('created_at DESC')
+    end
+
+    def show
+
     end
 
     def create
@@ -12,10 +17,30 @@ class Api::V1::CommentsController < Api::V1::BaseController
             else
                 render_error
             end
-        
+    end
+
+    def update
+        if @comment.update(comments_param)
+            render :show
+        else
+            render_error
+        end
+    end
+
+    def destroy
+        if @comment.destroy
+            render json: { success: "deleted" }
+        else
+            render json: { error: "error"}
+        end
+            # No need to create a `destroy.json.jbuilder` view
     end
 
     private
+
+    def set_comment
+        @comment = Comment.find(params[:id])
+    end
 
     def comments_param
         params.require(:comment).permit(:name, :image, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :restaurants, only: [ :index, :show, :update, :create, :destroy ] do
-        resources :comments, only: [:index]
+        resources :comments, only: [:index, :create]
       end
+
+      resources :comments, only: [:show, :update, :destroy]
     end
   end
 
