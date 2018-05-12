0bacfe94ca646084a4f44f18b440e7dcc9d6a20c
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-05-12 12:52:30 +0800

    update working .. with out user authen

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+    # acts_as_token_authentication_handler_for User, except: [ :index, :show ]
     before_action :set_restaurant, only: [:edit, :update, :show, :destory]
 
     def index
@@ -9,9 +10,27 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
 
     end
 
+    def update
+        if @restaurant.update(restaurant_params)
+            render :show
+        else
+            render_error
+        end
+    end
+
     private
 
     def set_restaurant
         @restaurant = Restaurant.find(params[:id])
     end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :address, :image, :description)
+    end
+
+    def render_error
+        render json: { errors: @restaurant.error.full_messages },
+            status: :unprocessable_entity
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show ]
+      resources :restaurants, only: [ :index, :show, :update ]
     end
   end
 
